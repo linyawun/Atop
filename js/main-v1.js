e583774b52d5732cdb505543845b90e0e6490faa
Nathan Cheng <37738465+NeeeeT@users.noreply.github.com>
2022-01-11 22:15:04 +0800

    fix hover bug.

--- js/main-v1.js
+++ js/main-v1.js
@@ -1,9 +1,9 @@
 function isMobile() {
-    var mobile_flag = 0;
-    var screen_width = window.screen.width;
-    var screen_height = window.screen.height;    
-    //  var screen_width = window.innerWidth;
-    //  var screen_height = window.innerHeight;   
+    let mobile_flag = 0;
+    let screen_width = window.screen.width;
+    let screen_height = window.screen.height;    
+    //  let screen_width = window.innerWidth;
+    //  let screen_height = window.innerHeight;   
      //根据屏幕分辨率判断是否是手机
      if(screen_width < 500 && screen_height < 820){
          mobile_flag = 1;
@@ -23,28 +23,21 @@ function isMobile() {
 }
 
 function hoverImg(){
-    var main = document.getElementById("main");
-    var imgright = document.getElementById("img-right");
-    var imgrightbg = document.getElementById("imgright-bg");
-    var imgleft = document.getElementById("img-left");
-    var imgleftbg = document.getElementById("imgleft-bg");
-    var moveL=110;
-    var moveS=100;
+    const main = document.getElementById("main");
+    const imgright = document.getElementById("img-right");
+    const imgrightbg = document.getElementById("imgright-bg");
+    const imgleft = document.getElementById("img-left");
+    const imgleftbg = document.getElementById("imgleft-bg");
+    let moveL=110;
+    let moveS=100;
+    // constants
+    let c_imgright_top = imgright.offsetTop;
+    let c_imgrightbg_top = imgrightbg.offsetTop;
+    let c_imgleft_left = imgleft.offsetLeft;
+    let c_imgleftbg_left = imgleftbg.offsetLeft;
     //hover右側區塊
     $(".right-block").hover(function(){
-        var imgright_top = imgright.offsetTop;
-        var imgright_right = main.offsetWidth - imgright.offsetLeft - imgright.offsetWidth;
-    
-        var imgrightbg_top = imgrightbg.offsetTop;
-        var imgrightbg_right = main.offsetWidth - imgrightbg.offsetLeft - imgrightbg.offsetWidth;
-        
-        var imgleft_bottom = main.offsetHeight - imgleft.offsetTop - imgleft.offsetHeight;
-        var imgleft_left = imgleft.offsetLeft;
-    
-        var imgleftbg_bottom = main.offsetHeight - imgleftbg.offsetTop - imgleftbg.offsetHeight;
-        var imgleftbg_left = imgleftbg.offsetLeft;
-    
-        var mobile_flag = isMobile(); // true為手機端，false為電腦端
+        let mobile_flag = isMobile(); // true為手機端，false為電腦端
         if(mobile_flag==0){//電腦
             moveL=110;
             moveS=100;
@@ -58,11 +51,25 @@ function hoverImg(){
             moveL=100;
             moveS=50;
         }
+        let imgright_top = imgright.offsetTop;
+        let imgright_right = main.offsetWidth - imgright.offsetLeft - imgright.offsetWidth;
+    
+        let imgrightbg_top = imgrightbg.offsetTop;
+        let imgrightbg_right = main.offsetWidth - imgrightbg.offsetLeft - imgrightbg.offsetWidth;
+        
+        let imgleft_bottom = main.offsetHeight - imgleft.offsetTop - imgleft.offsetHeight;
+        let imgleft_left = imgleft.offsetLeft;
+    
+        let imgleftbg_bottom = main.offsetHeight - imgleftbg.offsetTop - imgleftbg.offsetHeight;
+        let imgleftbg_left = imgleftbg.offsetLeft;
+        if(Math.abs(imgright_top - c_imgright_top) > 15){
+            return;
+        }
         //綠色漸層
         $( ".hover-bg-right" ).addClass( "hover-bg-open" ); 
 
         //放大圖片避免圖不夠大
-        $( ".img-content-right" ).addClass( "img-content-right-open" ); 
+        $( ".img-content-right" ).addClass( "img-content-right-open" );
         //區塊位移
         $(".img-right").css({
             'top': imgright_top+moveL,
@@ -90,22 +97,9 @@ function hoverImg(){
         imgrightbg.style.cssText = '';
         imgleftbg.style.cssText = '';
     });
-
     //hover左側區塊
     $(".left-block").hover(function(){
-        var imgright_top = imgright.offsetTop;
-        var imgright_right = main.offsetWidth - imgright.offsetLeft - imgright.offsetWidth;
-    
-        var imgrightbg_top = imgrightbg.offsetTop;
-        var imgrightbg_right = main.offsetWidth - imgrightbg.offsetLeft - imgrightbg.offsetWidth;
-        
-        var imgleft_bottom = main.offsetHeight - imgleft.offsetTop - imgleft.offsetHeight;
-        var imgleft_left = imgleft.offsetLeft;
-    
-        var imgleftbg_bottom = main.offsetHeight - imgleftbg.offsetTop - imgleftbg.offsetHeight;
-        var imgleftbg_left = imgleftbg.offsetLeft;
-    
-        var mobile_flag = isMobile(); // true為手機端，false為電腦端
+        let mobile_flag = isMobile(); // true為手機端，false為電腦端
         if(mobile_flag==0){//電腦
             moveL=110;
             moveS=100;
@@ -118,6 +112,17 @@ function hoverImg(){
         }else if(mobile_flag==3){
             moveL=100;
             moveS=50;
+        }
+        let imgright_top = imgright.offsetTop;
+        let imgright_right = main.offsetWidth - imgright.offsetLeft - imgright.offsetWidth;
+        let imgrightbg_top = imgrightbg.offsetTop;
+        let imgrightbg_right = main.offsetWidth - imgrightbg.offsetLeft - imgrightbg.offsetWidth;
+        let imgleft_bottom = main.offsetHeight - imgleft.offsetTop - imgleft.offsetHeight;
+        let imgleft_left = imgleft.offsetLeft;
+        let imgleftbg_bottom = main.offsetHeight - imgleftbg.offsetTop - imgleftbg.offsetHeight;
+        let imgleftbg_left = imgleftbg.offsetLeft;
+        if(Math.abs(imgright_top - c_imgright_top) > 15){
+            return;
         }
         //綠色漸層
         $( ".hover-bg-left" ).addClass( "hover-bg-open" );
